8ba31ea0f32317ac2f311be15f00be569220671f
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-04-29 01:27:30 +0300

    check

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status')
+def state():
+    """
+    Returns a JSON response with status OK.
+    """
+    return jsonify({"status": "OK"})
