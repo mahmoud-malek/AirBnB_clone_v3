5273b377a5bde143a8ad083bfbe97c6d9695e0e7
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-04-29 01:55:32 +0300

    check

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+main app
+"""
 import os
 from flask import Flask
 from models import storage
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+index
+"""
 from api.v1.views import app_views
 from flask import jsonify
 
