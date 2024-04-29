215efb5e72b7b8fc1c3c9ab24049fcf594563978
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-04-29 22:21:26 +0300

    check

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
+"""index file"""
+from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from api.v1.views import app_views
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {
+    "users": User,
+    "places": Place,
+    "states": State,
+    "cities": City,
+    "amenities": Amenity,
+    "reviews": Review
+}
 
 
 @app_views.route('/status')
@@ -19,17 +28,9 @@ def status():
 
 
 @app_views.route('/stats')
-def get_stats():
-    '''Gets the number of objects for each type.
-    '''
-    objects = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'states': State,
-        'users': User
-    }
-    for key, value in objects.items():
-        objects[key] = storage.count(value)
-    return jsonify(objects)
+def count_stats():
+    """Count stats"""
+    counter_of_stats = {}
+    for class_name, class_ in classes.items():
+        counter_of_stats[class_name] = storage.count(class_)
+    return jsonify(counter_of_stats)
